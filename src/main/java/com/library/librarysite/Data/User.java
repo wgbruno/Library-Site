2514b06f5ed6d4efa3aade1ddb234e778f596191
wgbruno <wgbruno@loyola.edu>
2023-12-01 16:20:07 -0500

    CRUD operations for user completed

--- src/main/java/com/library/librarysite/Data/User.java
+++ src/main/java/com/library/librarysite/Data/User.java
@@ -1,18 +1,30 @@
 package com.library.librarysite.Data;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private String name;
+    private String firstName;
+
+    private String lastName;
+
     private String email;
+
+    private String username;
+
+    private String password;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
     public Integer getId(){
         return id;
@@ -23,11 +35,19 @@ public class User {
     }
 
     public String getName() {
-        return name;
+        return firstName;
     }
 
-    public void setFirstName(String name){
-        this.name = name;
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail(){
@@ -36,5 +56,13 @@ public class User {
 
     public void setEmail(String email){
         this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
     }
 }
--- src/main/java/com/library/librarysite/Data/UserRepository.java
+++ src/main/java/com/library/librarysite/Data/UserRepository.java
@@ -1,6 +1,13 @@
 package com.library.librarysite.Data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findUserById(Integer id);
+
+    User deleteUserById(Integer id);
 }
